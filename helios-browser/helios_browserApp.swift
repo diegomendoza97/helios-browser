2ff52236c88cbdd012a6c0e8836088bda5efa87b
Diego Mendoza <diegocoronado_@outlook.com>
2026-02-18 18:37:17 -0800

    Initial Commit

---
+++ helios-browser/helios_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  helios_browserApp.swift
+//  helios-browser
+//
+//  Created by Diego Mendoza on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct helios_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
