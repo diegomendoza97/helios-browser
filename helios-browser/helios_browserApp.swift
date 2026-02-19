c14f66bc2b267c7ba59dc625c2dad69fdf2c5c9c
Diego Mendoza <diegocoronado_@outlook.com>
2026-02-18 19:13:56 -0800

    Initial work for Helios

--- helios-browser/helios_browserApp.swift
+++ helios-browser/helios_browserApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct helios_browserApp: App {
+    #if USE_CEF
+    @NSApplicationDelegateAdaptor(HeliosCEFAppDelegate.self) var cefAppDelegate
+    #endif
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BrowserView()
+                .frame(minWidth: 800, minHeight: 500)
+        }
+        .windowStyle(.automatic)
+        .defaultSize(width: 1100, height: 700)
+        .commands {
+            CommandGroup(replacing: .newItem) { }
         }
     }
 }
